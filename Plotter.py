ece53e1d7ff92047e2a1bb6c0cafd76b52b6fae7
ngoldfarb <nagoldfarb@wpi.edu>
2018-02-16 17:21:15 -0500

    updated code

--- Plotter.py
+++ Plotter.py
@@ -12,7 +12,7 @@ class Plotter():
 
     def __init__(self):
         self.fig = plt.figure()
-        ax = self.fig.add_subplot(111, autoscale_on=False, xlim=(-7, 7), ylim=(-7, 7))
+        ax = self.fig.add_subplot(111, autoscale_on=False, xlim=(-3, 3), ylim=(-3, 3))
         ax.grid()
 
         self.back_leg, = ax.plot([], [], 'bo-', lw=2)
@@ -22,12 +22,14 @@ class Plotter():
 
 
     def update(self, sub):
-        points = dynamics.FK(sub)
-        back = [ _.tolist() for _ in points[:-1]]
-        flat_back = [item for sublist in back for item in sublist]
+        points = sub.joints
 
-        self.back_leg.set_ydata([ flat_back[1], flat_back[3], flat_back[5], flat_back[7], flat_back[9] ])
-        self.back_leg.set_xdata([ flat_back[0], flat_back[2], flat_back[4], flat_back[6],flat_back[8] ])
+        #back = [ _.tolist() for _ in points[:-1]]
+        #flat_back = [item for sublist in back for item in sublist]
+        x,y =  zip(*points)
+
+        self.back_leg.set_ydata([ y[:-1] ])
+        self.back_leg.set_xdata([ x[:-1] ])
 
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
